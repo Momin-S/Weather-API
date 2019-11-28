6467bbe71064fd6ceff21ebcd393efa9e4a4608d
Momin-S <56354170+Momin-S@users.noreply.github.com>
2019-11-28 17:12:20 -0600

    Add files via upload

--- app.js
+++ app.js
@@ -4,17 +4,36 @@ var city = "Houston";
 var unit = "imperial";
 
 $.getJSON("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=" + unit + "&appid=9aa43f8529a545f9b4ecb4bd1eebb4ca", (data) => {
-  console.log(data)
+    console.log(data)
 
-  var icon = "http://openweathermap.org/img/wn/"+ data.weather[0].icon + ".png";
-  var temp = data.main.temp;
-  var weather = data.weather[0].main;
+    var icon = "http://openweathermap.org/img/wn/"+ data.weather[0].icon + ".png";
+    var temp = data.main.temp;
+    var weather = data.weather[0].main;
 
-  $('.city').append(city);
-  $('.icon').attr('src', icon);
-  $('.temp').append(Math.round(temp));
-  $('.weather').append(weather);
-});
+    document.getElementsByClassName("city")[0].innerHTML = city;
+    $('.icon').attr('src', icon);
+    document.getElementsByClassName("temp")[0].innerHTML = Math.round(temp);
+    document.getElementsByClassName("weather")[0].innerHTML = weather;
+  });
+
+function setCity() {
+  city = "" + document.getElementById("city_name").value + "";
+  $.getJSON("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=" + unit + "&appid=9aa43f8529a545f9b4ecb4bd1eebb4ca", (data) => {
+    console.log(data)
+
+    var icon = "http://openweathermap.org/img/wn/"+ data.weather[0].icon + ".png";
+    var temp = data.main.temp;
+    var weather = data.weather[0].main;
+
+    document.getElementsByClassName("city")[0].innerHTML = city;
+    $('.icon').attr('src', icon);
+    document.getElementsByClassName("temp")[0].innerHTML = Math.round(temp);
+    document.getElementsByClassName("weather")[0].innerHTML = weather;
+  });
+}
+
+
+
 
 function changeUnits() {
   console.log("Change units");
