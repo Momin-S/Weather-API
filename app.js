6f5db251bbbd0e9ba97b22908bc9bb28881edfc4
Momin-S <56354170+Momin-S@users.noreply.github.com>
2019-11-28 11:46:00 -0600

    Add files via upload

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 console.log("helloWorld");
 
-var city = "New York";
+var city = "Houston";
+var unit = "imperial";
 
-$.getJSON("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=9aa43f8529a545f9b4ecb4bd1eebb4ca", (data) => {
+$.getJSON("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=" + unit + "&appid=9aa43f8529a545f9b4ecb4bd1eebb4ca", (data) => {
   console.log(data)
 
   var icon = "http://openweathermap.org/img/wn/"+ data.weather[0].icon + ".png";
@@ -13,6 +14,26 @@ $.getJSON("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=i
   $('.icon').attr('src', icon);
   $('.temp').append(Math.round(temp));
   $('.weather').append(weather);
+});
 
+function changeUnits() {
+  console.log("Change units");
+
+  if (unit == 'imperial') {
+    unit = 'metric';
+    document.getElementsByClassName("temp_unit")[0].innerHTML = "° C";
+  } else {
+    unit = 'imperial';
+    document.getElementsByClassName("temp_unit")[0].innerHTML = "° F";
+  }
+
+  $.getJSON("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=" + unit + "&appid=9aa43f8529a545f9b4ecb4bd1eebb4ca", (data) => {
+
+  var temp = data.main.temp;
+
+  document.getElementsByClassName("temp")[0].innerHTML = Math.round(temp);
 
 });
+}
+
+
